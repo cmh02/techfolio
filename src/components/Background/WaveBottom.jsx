74b0ca729d93ea0ee556be1c4f66ed0be85fd476
mje0002 <mje0002@gmail.com>
2025-02-17 21:45:22 -0500

    Adding in the assets

---
+++ src/components/Background/WaveBottom.jsx
@@ -0,0 +1,17 @@
+const Bottom = () => {
+  return (
+    <svg
+      className="absolute bottom-0 w-full"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1440 320"
+    >
+      <path
+        fill="#3b82f6"
+        fillOpacity="1"
+        d="M0,256L80,234.7C160,213,320,171,480,160C640,149,800,171,960,186.7C1120,203,1280,213,1360,218.7L1440,224L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+      ></path>
+    </svg>
+  );
+};
+
+export default Bottom;
